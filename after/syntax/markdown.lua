14878b303c058f15b97942fcacf86cd082393e1a
bag <cpif@protonmail.com>
2022-06-18 02:26:19 -0600

    refactored md highlighting

---
+++ after/syntax/markdown.lua
@@ -0,0 +1,25 @@
+local basic = {
+  ctermfg='grey',
+  ctermbg='black'
+}
+
+local headers = {
+  ctermfg='white',
+  ctermbg='blue',
+  cterm = { bold=true }
+}
+
+vim.api.nvim_set_hl(0, 'Normal', basic)
+vim.api.nvim_set_hl(0, 'LineNr', basic)
+
+vim.api.nvim_set_hl(0, 'markdownH1', headers)
+vim.api.nvim_set_hl(0, 'markdownH2', headers)
+vim.api.nvim_set_hl(0, 'markdownH3', headers)
+vim.api.nvim_set_hl(0, 'markdownHeadingDelimiter', headers)
+
+vim.api.nvim_set_hl(0, 'markdownFootnote', {
+  ctermfg='green'})
+
+vim.api.nvim_set_hl(0, 'markdownUrl', {
+  ctermfg='lightgreen'})
+
--- after/syntax/markdown.vim
+++
@@ -1,8 +0,0 @@
-hi Normal                   ctermfg=grey        ctermbg=black
-hi LineNr                   ctermfg=lightgrey
-hi markdownFootnote         ctermfg=green
-hi markdownUrl              ctermfg=darkgreen
-hi markdownH1               ctermfg=yellow      cterm=underline
-hi markdownH2               ctermfg=yellow      cterm=underline
-hi markdownH3               ctermfg=yellow      cterm=underline
-hi markdownHeadingDelimiter ctermfg=yellow      cterm=underline

65ddaf3e546ab4f17f0721c016250755905f4af4
bag <cpif@protonmail.com>
2022-06-18 02:33:01 -0600

    markdown.lua

--- after/syntax/markdown.lua
+++ after/syntax/markdown.lua
@@ -9,17 +9,14 @@ local headers = {
   cterm = { bold=true }
 }
 
-vim.api.nvim_set_hl(0, 'Normal', basic)
-vim.api.nvim_set_hl(0, 'LineNr', basic)
+vim.api.nvim_set_hl(0, 'Normal',                    basic)
+vim.api.nvim_set_hl(0, 'LineNr',                    basic)
 
-vim.api.nvim_set_hl(0, 'markdownH1', headers)
-vim.api.nvim_set_hl(0, 'markdownH2', headers)
-vim.api.nvim_set_hl(0, 'markdownH3', headers)
-vim.api.nvim_set_hl(0, 'markdownHeadingDelimiter', headers)
+vim.api.nvim_set_hl(0, 'markdownH1',                headers)
+vim.api.nvim_set_hl(0, 'markdownH2',                headers)
+vim.api.nvim_set_hl(0, 'markdownH3',                headers)
+vim.api.nvim_set_hl(0, 'markdownHeadingDelimiter',  headers)
 
-vim.api.nvim_set_hl(0, 'markdownFootnote', {
-  ctermfg='green'})
-
-vim.api.nvim_set_hl(0, 'markdownUrl', {
-  ctermfg='lightgreen'})
+vim.api.nvim_set_hl(0, 'markdownFootnote', { ctermfg='green'})
+vim.api.nvim_set_hl(0, 'markdownUrl', { ctermfg='lightgreen'})
 

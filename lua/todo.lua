0367d33c219cb2d5c5bbdc723268296301049bea
bag <cpif@protonmail.com>
2022-06-27 14:56:20 -0600

    set todolist tw to 0

--- lua/todo.lua
+++ lua/todo.lua
@@ -3,15 +3,12 @@ local todobuf, todowin
 function opentodo()
   todobuf = vim.api.nvim_create_buf(false, false)
 
-
   local width      = vim.api.nvim_get_option("columns")
   local height     = vim.api.nvim_get_option("lines")
   local win_width  = math.ceil(width  * 0.6)
   local win_height = math.ceil(height * 0.6 - 4)
   local col        = math.ceil((width  - win_width ) / 2)
   local row        = math.ceil((height - win_height) / 2 - 1)
-
-  vim.api.nvim_buf_set_option(todobuf, 'textwidth', win_width - 2)
 
   local opts = {
     style    = "minimal",
@@ -25,6 +22,7 @@ function opentodo()
 
   todowin = vim.api.nvim_open_win(todobuf, true, opts)
   vim.cmd('edit ' .. todofile)
+  vim.api.nvim_buf_set_option(todobuf, 'textwidth', 0)
 end
 
 function closetodo()

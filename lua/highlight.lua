bef46fd5121b6f3873d4936e6d42fb588db1087b
bag <cpif@protonmail.com>
2022-07-14 15:05:58 -0600

    Create basic highlighting script

---
+++ lua/highlight.lua
@@ -0,0 +1,36 @@
+-- PROGRAMMATICALLY SET HIGHLIGHTS
+function sethi(specs, cats)
+  for i,v in ipairs(cats) do
+    vim.api.nvim_set_hl(0, v, specs)
+  end
+end
+
+-- NORMAL {{{
+local normal	= {
+  foreground  = 'black',
+  background  = '#eeeeee',
+  ctermfg     = 'grey',
+  ctermbg     = 'black'
+}
+
+local normalh = {
+  'Normal', 'LineNr', 'Pmenu', 'FloatBorder',
+}
+
+sethi(normal, normalh)
+-- }}}
+
+-- FOLDS {{{
+local fold    = {
+  foreground  = 'black',
+  background  = '#eeeeee',
+  ctermfg     = 'black',
+  ctermbg     = 'darkcyan'
+}
+
+local foldh   = {
+  'Folded', 'FoldColumn',
+}
+
+sethi(normal, normalh)
+-- }}}

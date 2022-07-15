3c8948556fa3a6f83d4c811e2405760d9d0ee250
bag <cpif@protonmail.com>
2022-07-15 03:50:36 -0600

    Annotated `checkbox`

--- lua/checkbox.lua
+++ lua/checkbox.lua
@@ -1,3 +1,13 @@
+--[[
+
+> Lose no time; be always employ'd in something useful; cut off
+> all unnecessary actions.
+                                             -- Benjamin Franklin
+
+TODO: make this script handle indented checkboxes.
+
+]]
+
 function checkuncheck(checkline)
   if string.sub(checkline, 4, 4) == " " then
     return string.sub(checkline, 1, 3) .. 'x' .. string.sub(checkline, 5, -1)

5e023f88e85e263dbbab383a13c5d32344bc1a46
bag <cpif@protonmail.com>
2022-07-16 02:57:08 -0600

    Fixed a little problem with swapfiles

--- init.lua
+++ init.lua
@@ -154,7 +154,7 @@ vim.keymap.set('n', '<leader>d',  function()
 end)
 
 -- Hide those pesky swapfiles
-local backups   = '~/.vim-temp//'
+local backups   = vim.fn.expand('~/.vim-temp//')
 vim.o.backupdir = backups
 vim.o.directory = backups
 vim.o.undodir   = backups

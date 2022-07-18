5d8bc6430a1ca75dc7a5ff892bbff39517b15154
bag <cpif@protonmail.com>
2022-07-18 00:00:57 -0600

    updated syntax highlighting
    - simplified highlighting function
    - added the basic highlighting types to `lua/highlight` -- including gui
      colors

--- after/syntax/markdown.lua
+++ after/syntax/markdown.lua
@@ -1,17 +1,17 @@
-local headers  = {
-  ctermfg      ='white',
-  ctermbg      ='blue',
-  cterm        = {
-    bold       = true,
+local headers     = {
+  specs           = {
+    ctermfg       ='white',
+    ctermbg       ='blue',
+    cterm         = {
+      bold        = true,
+    },
+  },
+  categories      = {
+    'markdownH1', 'markdownH2', 'markdownH3',
+    'markdownHeadingDelimiter',
   }
 }
-
-local headersh = {
-  'markdownH1', 'markdownH2', 'markdownH3',
-  'markdownHeadingDelimiter',
-}
-
-sethi(headers, headersh)
+sethi(headers)
 
 vim.api.nvim_set_hl(0, 'markdownFootnote', { ctermfg='green'})
 vim.api.nvim_set_hl(0, 'markdownUrl', { ctermfg='lightgreen'})
--- lua/highlight.lua
+++ lua/highlight.lua
@@ -1,36 +1,117 @@
--- PROGRAMMATICALLY SET HIGHLIGHTS
-function sethi(specs, cats)
-  for i,v in ipairs(cats) do
-    vim.api.nvim_set_hl(0, v, specs)
+-- Programmatically set highlights
+function sethi(highlights)
+  for i,v in ipairs(highlights.categories) do
+    vim.api.nvim_set_hl(0, v, highlights.specs)
   end
 end
 
--- NORMAL {{{
-local normal	= {
-  foreground  = 'black',
-  background  = '#eeeeee',
-  ctermfg     = 'grey',
-  ctermbg     = 'black'
+-- Normal         {{{
+local normal	    = {
+  specs           = {
+    foreground    = 'black',
+    background    = '#eeeeee',
+    ctermfg       = 'grey',
+    ctermbg       = 'black'
+  },
+  categories      = {
+    'Normal', 'LineNr', 'Pmenu', 'FloatBorder',
+  }
 }
-
-local normalh = {
-  'Normal', 'LineNr', 'Pmenu', 'FloatBorder',
-}
-
-sethi(normal, normalh)
+sethi(normal)
 -- }}}
 
--- FOLDS {{{
-local fold    = {
-  foreground  = 'black',
-  background  = '#eeeeee',
-  ctermfg     = 'black',
-  ctermbg     = 'darkcyan'
+-- Folds          {{{
+local fold        = {
+  specs           = {
+    foreground    = 'black',
+    background    = '#eeeeee',
+    ctermfg       = 'black',
+    ctermbg       = 'darkcyan'
+  },
+  categories      = {
+    'Folded', 'FoldColumn',
+  }
 }
-
-local foldh   = {
-  'Folded', 'FoldColumn',
-}
-
-sethi(fold, foldh)
+sethi(fold)
 -- }}}
+
+-- Comment        {{{
+local Comment     = {
+  specs           = {
+    default       = true,
+    foreground    = '#6070a0',
+  },
+  categories      = {
+    'Comment',  
+  }
+}
+sethi(Comment)
+-- }}}
+
+-- Special        {{{
+local Special     = {
+  specs           = {
+    default       = true,
+    foreground    = '#bb00bb',
+  },
+  categories      = {
+    'Special',  
+  }
+}
+sethi(Special)
+-- }}}
+
+-- Type           {{{
+local Type        = {
+  specs           = {
+    default       = true,
+    foreground    = '#30c030',
+		bold      	  = true,
+  },
+  categories      = {
+    'Type',
+  }
+}
+sethi(Type)
+-- }}}
+
+-- Statement      {{{
+local Statement   = {
+  specs           = {
+    default       = true,
+    foreground    = '#aaaa40',
+		bold      	  = true,
+  },
+  categories      = {
+    'Statement',
+  }
+}
+sethi(Statement)
+-- }}}
+
+-- Identifier     {{{
+local Identifier  = {
+  specs           = {
+    default       = true,
+    foreground    = '#30b0c0',
+  },
+  categories      = {
+    'Identifier',
+  }
+}
+sethi(Identifier)
+-- }}}
+
+-- Constant       {{{
+local Constant    = {
+  specs           = {
+    default       = true,
+    foreground    = '#b02020',
+  },
+  categories      = {
+    'Constant',
+  }
+}
+sethi(Constant)
+-- }}}
+

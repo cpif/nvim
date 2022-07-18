b380b1c42b41f043149cac99c291e26b511f2088
bag <cpif@protonmail.com>
2022-07-18 12:46:55 -0600

    reorganized functions

--- after/syntax/markdown.lua
+++ after/syntax/markdown.lua
@@ -1,10 +1,3 @@
-local headercolors = {
-  { name = 'H1', cterm = 'Blue', },
-  { name = 'H2', cterm = 'Magenta', },
-  { name = 'H3', cterm = 'DarkGreen', },
-}
-setmdheaders(headercolors)
-
 -- Programmatically set markdown header colors {{{
 function setmdheaders(headercolors)
   for i,v in ipairs(headercolors) do
@@ -27,6 +20,13 @@ function setmdheaders(headercolors)
   end
 end
 -- }}}
+
+local headercolors = {
+  { name = 'H1', cterm = 'Blue', },
+  { name = 'H2', cterm = 'Magenta', },
+  { name = 'H3', cterm = 'DarkGreen', },
+}
+setmdheaders(headercolors)
 
 vim.api.nvim_set_hl(0, 'markdownFootnote', { ctermfg='green'})
 vim.api.nvim_set_hl(0, 'markdownUrl', { ctermfg='lightgreen'})

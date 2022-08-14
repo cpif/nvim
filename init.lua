654769ad0e7bb9546bfa8add3bd2cc1e5cf86ab7
bag <cpif@protonmail.com>
2022-08-13 23:30:15 -0400

    remove unnecessary spaces

--- init.lua
+++ init.lua
@@ -1,10 +1,8 @@
 --[[
-
 > I was never embarrassed about asking someone, literally, "What
 > should I paint?" because how is asking someone for ideas any
 > different from looking for them in a magazine? 
                                                     --Andy Warhol
-
 --]]
 
 require('filetypes')

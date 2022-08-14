7fc8b51a7edd52a991ddd5ff391c15ddb05abdfd
cpif <cpif@protonmail.com>
2022-08-13 21:50:04 -0400

    indented todos fix

--- lua/checkbox.lua
+++ lua/checkbox.lua
@@ -3,18 +3,19 @@
 > Lose no time; be always employ'd in something useful; cut off
 > all unnecessary actions.
                                              -- Benjamin Franklin
-
-TODO: make this script handle indented checkboxes.
-
 ]]
 
 function checkuncheck(checkline)
-  if string.sub(checkline, 4, 4) == " " then
+  if string.sub(checkline, 3, 5) == '[ ]' then
     return string.sub(checkline, 1, 3) .. 'x' .. string.sub(checkline, 5, -1)
-  elseif string.sub(checkline, 4, 4) == "x" then
+  elseif string.sub(checkline, 3, 5) == '[x]' then
     return string.sub(checkline, 1, 3) .. ' ' .. string.sub(checkline, 5, -1)
+  elseif string.sub(checkline, 7, 9) == '[ ]' then
+    return string.sub(checkline, 1, 7) .. 'x' .. string.sub(checkline, 9, -1)
+  elseif string.sub(checkline, 7, 9) == '[x]' then
+    return string.sub(checkline, 1, 7) .. ' ' .. string.sub(checkline, 9, -1)
   else
-    print("Is that a checkbox?")
+    print('Is that a checkbox?')
     return checkline
   end
 end

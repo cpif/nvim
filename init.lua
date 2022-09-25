9b88a491e9dae373366e15d90f049da7e6a7e8d2
cpif <cpif@protonmail.com>
2022-09-25 15:56:35 -0400

    support for ctags (read: my memory)

--- init.lua
+++ init.lua
@@ -4,6 +4,7 @@
 > different from looking for them in a magazine? 
                                                 -- Andy Warhol ]]
 require('abbreviations')
+require('bibliography')
 require('citations')
 require('cursor')
 require('filetypes')
@@ -64,7 +65,7 @@ mapfloatingwindow({
   leader      = 'b',          -- Press <leader> then "b" to open float
   scratch     = false,        -- "Scratch" will make buffer delete instantly
   size        = { 0.8, 0.8 }, -- Width ratio, height ratio
-  buffer      = 0,            -- Buffer number (initialized to zero)
+  buffer      = 0,            -- Buffer number (initialized to zero, will be updated by the function)
   window      = 0,            -- Buffer window (initialized to zero)
   write       = true,         -- Should the buffer be written?
   options     = {}            -- Table of buffer-local options
@@ -122,6 +123,9 @@ end)
 
 -- Add blank line above current line
 vim.keymap.set('n', '<leader>o', 'O<esc>0D')
+
+-- Run ctags on bibliography
+vim.keymap.set('n', '<leader>t', function() bibtags() end)
 
 -- I love the `g-<ctrl-a>` command, but tmux eats it.
 vim.keymap.set('v', '<leader>a', 'g<C-a>')
---
+++ lua/bibliography.lua
@@ -0,0 +1,12 @@
+--[[
+
+Functions related to the bibtex bibliography file I maintain.
+
+--]]
+
+-- I can't remember, and shouldn't have to remember, the annoying
+-- options from ctags.
+
+function bibtags()
+  vim.cmd("!ctags --language-force=bibtex --kinds-bibtex=e ~/Bibliography/bibliography.bib 2> /dev/null")
+end

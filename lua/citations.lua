401c5997773dd6d0c429dd0876107e2bf3d6dbdb
cpif <cpif@protonmail.com>
2022-11-10 11:48:50 -0500

    cleanup

--- lua/citations.lua
+++
@@ -1,8 +0,0 @@
--- Make the tagfile from the bibliography globally available
---
--- The vim docs say to set `tags` with `+=`, so I grab the
--- content of `vim.o.tags` and couple it (with a comma) to my
--- master bibliography file's tags.
-local bibtags = vim.o.tags .. ',' ..
-                vim.fn.expand('~/Bibliography/tags')
-vim.o.tags    = bibtags

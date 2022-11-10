bdfa113a2ad61d43c5491079373433c9a06ec076
cpif <cpif@protonmail.com>
2022-11-10 11:49:00 -0500

    cleanup

--- lua/bibliography.lua
+++ lua/bibliography.lua
@@ -10,3 +10,12 @@ Functions related to the bibtex bibliography file I maintain.
 function bibtags()
   vim.cmd("silent !ctags -f ~/Bibliography/tags --language-force=bibtex --kinds-bibtex=e ~/Bibliography/bibliography.bib 2> /dev/null")
 end
+
+-- Make the tagfile from the bibliography globally available
+
+-- The vim docs say to set `tags` with `+=`, so I grab the
+-- content of `vim.o.tags` and couple it (with a comma) to my
+-- master bibliography file's tags.
+local bibtags = vim.o.tags .. ',' ..
+                vim.fn.expand('~/Bibliography/tags')
+vim.o.tags    = bibtags

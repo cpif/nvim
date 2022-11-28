e9f92240e135a2f29ae032c09aeb0e5d9d1441d2
cpif <cpif@protonmail.com>
2022-11-28 10:39:25 -0500

    Created an additional master bibliography

--- lua/bibliography.lua
+++ lua/bibliography.lua
@@ -1,14 +1,18 @@
 --[[
 
-Functions related to the bibtex bibliography file I maintain.
+Functions related to the bibtex bibliography files I maintain.
+
+`bibtags()` makes a tagfile without having to remember all the
+annoying ctags options.
+
+I also need to set nvim's `tags` option so that these tag files
+are globally available.
 
 --]]
 
--- I can't remember, and shouldn't have to remember, the annoying
--- options from ctags.
-
 function bibtags()
   vim.cmd('silent !ctags -f ~/Bibliography/tags --language-force=bibtex --kinds-bibtex=e ~/Bibliography/bibliography.bib 2> /dev/null')
+  vim.cmd('silent !ctags -f ~/Dissertation/tags --language-force=bibtex --kinds-bibtex=e ~/Dissertation/bibliography.bib 2> /dev/null')
 end
 
 -- Make the tagfile from the bibliography globally available
@@ -18,4 +22,5 @@ end
 -- master bibliography file's tags.
 local bibtags = vim.o.tags .. ',' ..
                 vim.fn.expand('~/Bibliography/tags')
+                vim.fn.expand('~/Dissertation/tags')
 vim.o.tags    = bibtags

cd431339f0fc770d4481e18f2facfa8053108190
parg <cpif@protonmail.com>
2022-11-30 19:25:38 -0500

    Move options to its own file

---
+++ lua/opts.lua
@@ -0,0 +1,17 @@
+local opts          = vim.o
+
+opts.linebreak      = true
+opts.textwidth      = 65
+opts.autoindent     = true
+opts.rnu            = true
+opts.hlsearch       = false
+opts.foldlevelstart = 0
+opts.foldmethod     = 'marker'
+opts.expandtab      = true
+opts.tabstop        = 4
+opts.shiftwidth     = 4
+opts.guifont        = "Courier:h16"
+opts.dictionary     = '/usr/share/dict/american-english'
+opts.equalprg       = 'pandoc'
+opts.ignorecase     = true
+opts.smartcase      = true

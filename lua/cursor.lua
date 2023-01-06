beb3649bcf4e232af17ee5fca9580a893f371d89
bag <cpif@protonmail.com>
2023-01-05 21:02:47 -0700

    Remove files
    As part of a rationalization of my lua `require` calls

--- lua/cursor.lua
+++
@@ -1,5 +0,0 @@
--- I cribbed this from the docs, but I don't really know why it
--- works.
-
-vim.go.guicursor =
-  'i-ci:ver30-iCursor-blinkwait500-blinkon250-blinkoff250'
--- lua/startdir.lua
+++
@@ -1,4 +0,0 @@
-local startdir = vim.fn.getcwd()
-if startdir == vim.fn.expand('~\\NVIM\\nvim-win64\\bin') then
-  vim.cmd('cd ' .. vim.fn.expand('~\\Dropbox\\note'))
-end

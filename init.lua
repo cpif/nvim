bcd4d5277b52fe2aea5c370165ae73882157aaf2
bag <cpif@protonmail.com>
2023-01-05 20:16:15 -0700

    opts

--- init.lua
+++ init.lua
@@ -12,26 +12,12 @@ require('highlight')
 require('lines')
 require('lorem')
 require('netrw')
+require('opts')
 require('plug')
 require('startdir')
 require('status')
 require('swapfiles')
 
-vim.o.linebreak       = true
-vim.o.textwidth       = 65
-vim.o.autoindent      = true
-vim.o.rnu             = true
-vim.o.hlsearch        = false
-vim.o.foldlevelstart  = 0
-vim.o.foldmethod      = 'marker'
-vim.o.expandtab       = true
-vim.o.tabstop         = 4
-vim.o.shiftwidth      = 4
-vim.o.guifont         = "Courier:h16"
-vim.o.dictionary      = '/usr/share/dict/american-english'
-vim.o.equalprg        = 'pandoc'
-vim.o.ignorecase      = true
-vim.o.smartcase       = true
 
 --[[ Floating Windows                                         {{{
 
---
+++ lua/opts.lua
@@ -0,0 +1,16 @@
+vim.o.linebreak       = true
+vim.o.textwidth       = 65
+vim.o.autoindent      = true
+vim.o.rnu             = true
+vim.o.hlsearch        = false
+vim.o.foldlevelstart  = 0
+vim.o.foldmethod      = 'marker'
+vim.o.expandtab       = true
+vim.o.tabstop         = 4
+vim.o.shiftwidth      = 4
+vim.o.guifont         = "Courier:h16"
+vim.o.dictionary      = '/usr/share/dict/american-english'
+vim.o.equalprg        = 'pandoc'
+vim.o.ignorecase      = true
+vim.o.smartcase       = true
+

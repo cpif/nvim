c68719676df45fbdd8afdfdbefeda7a93e9c7646
cpif <cpif@protonmail.com>
2023-04-13 18:01:57 -0400

    fixed paths for bibliography

--- lua/bibliography.lua
+++ lua/bibliography.lua
@@ -1,17 +1,17 @@
 function bibtags()
   vim.cmd([[ silent !ctags 
-           \ -f ~/bibliography/tags
+           \ -f ~/Bibliography/tags
            \ --language-force=bibtex
-           \ --kinds-bibtex=e
-           \ ~/bibliography/bibliography.bib 2> /dev/null ]])
+           \ --kinds-bibtex=abiI
+           \ ~/Bibliography/bibliography.bib 2> /dev/null ]])
   vim.cmd([[ silent !ctags
            \ -f ~/Dissertation/tags
            \ --language-force=bibtex
-           \ --kinds-bibtex=e
+           \ --kinds-bibtex=abiI
            \ ~/Dissertation/bibliography.bib 2> /dev/null ]])
 end
 
 local bibtags = vim.o.tags .. ',' ..
-                vim.fn.expand('~/bibliography/tags') .. ',' ..
+                vim.fn.expand('~/Bibliography/tags') .. ',' ..
                 vim.fn.expand('~/Dissertation/tags')
 vim.o.tags    = bibtags

237748dec62c5ea7089cda2a82cdbbff2f969586
cpif <cpif@protonmail.com>
2023-04-24 16:58:00 -0400

    better fold highlighting

--- lua/highlight.lua
+++ lua/highlight.lua
@@ -4,7 +4,7 @@ local hls = {
   NormalFloat = { ctermfg = 7,  ctermbg = 0 },
   SpellBad    = { ctermfg = 11, bold = true },
   SpellCap    = { ctermfg = 11, bold = true },
-  Folded      = { ctermfg = 7,  ctermbg = 8 },
+  Folded      = { ctermfg = 0,  ctermbg = 4 },
 }
 
 for k,v in pairs(hls) do

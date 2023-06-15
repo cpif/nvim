8b12ef36f06b60ef1c2e7aae3f63a665fea620f9
parg <git@cpif.top>
2023-06-15 07:22:28 -0600

    add some abbrevs

--- lua/abbreviations.lua
+++ lua/abbreviations.lua
@@ -1,2 +1,5 @@
 -- No capitalizing
 vim.cmd('cnorea myvimrc $MYVIMRC', false)
+vim.cmd('inorea precuation precaution', false)
+vim.cmd('inorea precuations precautions', false)
+vim.cmd('ia knoweldge knowledge', false)

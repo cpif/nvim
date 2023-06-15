d3e2f417319d19aec7e94b64cc271805a54e5f2f
parg <git@cpif.top>
2023-06-15 07:23:47 -0600

    mappings

--- lua/maps.lua
+++ lua/maps.lua
@@ -64,3 +64,71 @@ vim.keymap.set('n', '<leader>d',  function()
 end)
 -- }}}
 
+--[[ Floating Windows                                         {{{
+
+The following section creates mappings for "floating windows" --
+which the nvim documentation says are "useful to implement simple
+widgets, such as tooltips displayed next to the cursor." Indeed
+they are.
+
+I have "implemented" a few "widgets" here:
+
+- A bibliography;
+- A todo list;
+- A scratchpad for taking on-the-fly notes that instantly delete
+  themselves.
+
+To see the functions that generate these floating windows, go to:
+lua/floatingwindows.lua
+
+To get a sense of the tables that get passed to those functions
+work, look a few lines down at the commented "bibliography"
+table.
+
+]]
+
+require('floatingwindows')
+
+-- The following table contains everything you need to make a
+-- floating window.
+mapfloatingwindow({
+  file        = '~/bibliography/bibliography.bib',
+  open        = false,        -- The float is not open... yet
+  leader      = 'b',          -- Press <leader> then "b" to open float
+  scratch     = false,        -- "Scratch" will make buffer delete instantly
+  size        = { 0.8, 0.8 }, -- Width ratio, height ratio
+  buffer      = 0,            -- Buffer number (initialized to zero, will be updated by the f unction)
+  window      = 0,            -- Buffer window (initialized to zero)
+  write       = true,         -- Should the buffer be written?
+  options     = {}            -- Table of buffer-local options
+})
+
+mapfloatingwindow({
+  file        = '~/wiki/todo.md',
+  open        = false,
+  leader      = 'l',
+  scratch     = false,
+  size        = { 0.6, 0.6 },
+  buffer      = 0,
+  window      = 0,
+  write       = true,
+  options     = {
+    textwidth = 0,
+  }
+})
+
+mapfloatingwindow({
+  file        = 'scratch',
+  open        = false,
+  leader      = 's',
+  scratch     = true,
+  size        = { 0.8, 0.8 },
+  buffer      = 0,
+  window      = 0,
+  write       = false,
+  options     = {
+    textwidth = 0,
+  }
+})
+
+--}}}

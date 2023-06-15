91079d929d20fa7bbc2554e552fb59469283006e
parg <git@cpif.top>
2023-06-15 07:24:09 -0600

    add options

--- lua/opts.lua
+++ lua/opts.lua
@@ -8,17 +8,20 @@ vim.o.foldmethod      = 'marker'
 vim.o.expandtab       = true
 vim.o.tabstop         = 4
 vim.o.shiftwidth      = 4
-vim.o.guifont         = "Courier:h16"
+vim.o.guifont         = 'Nimbus Mono:h10'
 vim.o.dictionary      = '/usr/share/dict/american-english'
 vim.o.equalprg        = 'pandoc'
 vim.o.ignorecase      = true
 vim.o.smartcase       = true
+vim.o.grepprg         = 'rg -i --vimgrep $*'
+-- vim.o.formatoptions   = 'jcroql'
 
 local backups         = vim.fn.expand('~/.vim-temp//')
 vim.o.backupdir       = backups
 vim.o.directory       = backups
 vim.o.undodir         = backups
 
+vim.g.neovide_scroll_animation_length = 0.9
 vim.go.guicursor =
   'i-ci:ver30-iCursor-blinkwait500-blinkon250-blinkoff250'
 
@@ -28,3 +31,8 @@ vim.g.vimwiki_list = {{
   ext     = 'md',
 }}
 vim.g.vimwiki_global_ext = 0
+
+vim.g.markdown_fenced_languages = {
+  'awk', 'lua', 'perl', 'html',
+  'sh', 'bash', 'bib',
+}

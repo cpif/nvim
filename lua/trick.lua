9b7d869d8b9f0ed838c266819a5d8dc0e2f987f2
parg <git@cpif.top>
2023-07-09 12:20:11 -0600

    change checkboxes

--- lua/trick.lua
+++ lua/trick.lua
@@ -5,12 +5,12 @@
 
 function checkuncheck(checkline)
   if string.sub(checkline, 3, 5) == '[ ]' then
-    return string.sub(checkline, 1, 3) .. 'x' .. string.sub(checkline, 5, -1)
-  elseif string.sub(checkline, 3, 5) == '[x]' then
+    return string.sub(checkline, 1, 3) .. 'X' .. string.sub(checkline, 5, -1)
+  elseif string.sub(checkline, 3, 5) == '[X]' then
     return string.sub(checkline, 1, 3) .. ' ' .. string.sub(checkline, 5, -1)
   elseif string.sub(checkline, 7, 9) == '[ ]' then
-    return string.sub(checkline, 1, 7) .. 'x' .. string.sub(checkline, 9, -1)
-  elseif string.sub(checkline, 7, 9) == '[x]' then
+    return string.sub(checkline, 1, 7) .. 'X' .. string.sub(checkline, 9, -1)
+  elseif string.sub(checkline, 7, 9) == '[X]' then
     return string.sub(checkline, 1, 7) .. ' ' .. string.sub(checkline, 9, -1)
   else
     print('Is that a checkbox?')
